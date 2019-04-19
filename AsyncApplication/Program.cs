7248f83d8eb72f139a06b614d779d757ee365e1c
Евгений <trewor.92@gmail.com>
2019-04-19 17:31:46 +0400

    task-8 initial commit

---
+++ AsyncApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace AsyncApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tasks.GetMD5Async(new Uri("https://www.yandex.ru"));
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+
+
+}
---
+++ AsyncApplication/Tasks.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncApplication
+{
+    public static class Tasks
+    {
+        
+
+
+        /// <summary>
+        /// Returns the content of required uris.
+        /// Method has to use the synchronous way and can be used to compare the performace of sync \ async approaches-проход. 
+        /// </summary>
+        /// <param name="uris">Sequence of required uri</param>
+        /// <returns>The sequence of downloaded url content</returns>
+        public static IEnumerable<string> GetUrlContent(this IEnumerable<Uri> uris)
+        {
+
+           
+
+            WebClient client = new WebClient();
+            return uris.Select(x=> client.DownloadString(x));
+
+            // TODO : Implement GetUrlContent
+            //throw new NotImplementedException();
+        }
+
+
+
+        /// <summary>
+        /// Returns the content of required uris.
+        /// Method has to use the asynchronous way and can be used to compare the performace of sync \ async approaches. 
+        /// 
+        /// maxConcurrentStreams parameter should control the maximum of concurrent streams that are running at the same time (throttling). 
+        /// </summary>
+        /// <param name="uris">Sequence of required uri</param>
+        /// <param name="maxConcurrentStreams">Max count of concurrent request streams</param>
+        /// <returns>The sequence of downloaded url content</returns>
+        public static  IEnumerable<string> GetUrlContentAsync(this IEnumerable<Uri> uris, int maxConcurrentStreams)
+        {
+          //  WebClient client = new WebClient();
+            Semaphore sm = new Semaphore(maxConcurrentStreams, maxConcurrentStreams);           
+
+
+            var res = uris.Select(async x =>
+           {
+               sm.WaitOne();
+               var result =   await new WebClient().DownloadStringTaskAsync(x);
+               sm.Release();
+               return result;
+            });
+
+            Task.WhenAll(res);
+
+            return res.Select(x => x.Result);             
+            // TODO : Implement GetUrlContentAsync
+            //throw new NotImplementedException();
+        }
+
+       
+
+
+        /// <summary>
+        /// Calculates MD5 hash of required resource.
+        /// 
+        /// Method has to run asynchronous. 
+        /// Resource can be any of type: http page, ftp file or local file.
+        /// </summary>
+        /// <param name="resource">Uri of resource</param>
+        /// <returns>MD5 hash</returns>
+        public static async Task<string> GetMD5Async(this Uri resource)
+        {
+            var md5 = MD5.Create();
+            var stream = await new WebClient().OpenReadTaskAsync(resource);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = await stream.ReadAsync(buffer, 0, 4096);
+                if (bytesRead == 0) break;
+                md5.TransformBlock(buffer, 0, bytesRead, null, 0);
+            }
+
+
+            md5.TransformFinalBlock(new byte[0], 0, 0);
+
+            return BitConverter.ToString(md5.Hash).Replace("-", "").ToLowerInvariant();
+
+            // TODO : Implement GetMD5Async
+            //throw new NotImplementedException();
+        }
+    }
+
+}
